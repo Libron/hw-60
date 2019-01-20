95253b5a2a5d6ce98f23fca15a059b0aec18ee3e
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-20 13:28:47 +0600

    Fixed issue with fetching data and updating State

--- src/App.js
+++ src/App.js
@@ -10,44 +10,61 @@ class App extends Component {
   };
 
   endpointURL = 'http://146.185.154.90:8000/messages';
-  internal = null;
+  interval = null;
 
-  componentDidMount() {
-      fetch(this.endpointURL).then(response => {
-         if (response.ok) {
-             return response.json();
-         }
-         throw new Error('Request failed');
-      }).then(messages => {
-          const datetime = messages[messages.length - 1].datetime;
-          console.log(datetime);
-          this.setState({messages, datetime});
+
+  getNewMessages(datetime = this.state.datetime) {
+      console.log('[Method] getNewMessages()');
+      let url = this.endpointURL;
+      if (datetime !== null) {
+          url = this.endpointURL + '?datetime=' + datetime;
+          console.log('[Datetime]', datetime);
+          console.log('[URL]', url);
+      }
+
+      fetch(url).then(response => {
+          if (response.ok) {
+              return response.json();
+          }
+          throw new Error('Request failed');
+      }).then(result => {
+          console.log('[Result]', result);
+          if (result.length !== 0) {
+              console.log('[DATA]', result);
+              const messages = [
+                  ...this.state.messages,
+                  ...result
+              ];
+              const datetime = result[result.length - 1].datetime;
+              this.setState({messages, datetime});
+          }
+
+          console.log('[State]', this.state.messages);
       });
-      // then(() => {
-      //     fetch(this.endpointURL+'/messages?datetime='+this.state.datetime).then(response=>{
-      //         if (response.ok) {
-      //             return response.json();
-      //         }
-      //         throw new Error('Request failed');
-      //     });
-      //     console.log(this.state.datetime);
-      // });
   };
 
-    someHandler = (formData) => {
+
+  componentDidMount() {
+      this.interval = setInterval(() => {
+          this.getNewMessages()
+      }, 3000);
+      console.log('Interval:', this.interval);
+  };
+
+    publishMessage = (formData) => {
         const data = new URLSearchParams();
         data.set('message', formData.message);
         data.set('author', formData.author);
         fetch(this.endpointURL, {
            method: 'POST',
            body: data,
-        }).then(console.log('All is ok'));
+        }).then(console.log('New message published'));
     };
 
   render() {
     return (
       <div className="App">
-        <Form publishMessage={this.someHandler} />
+        <Form publishMessage={this.publishMessage} />
           <ChatBox messages={this.state.messages}/>
       </div>
     );
